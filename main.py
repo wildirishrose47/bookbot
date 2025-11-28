7b9b3f876e9ff2fcc7476ecff4b345c9244afbaa
wildirishrose47 <wildirishrose114@gmail.com>
2025-11-28 18:08:29 -0500

    get_book_text in main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("greetings boots")
+from unittest.main import main
+
+
+def get_book_text(filepath):
+    with open(filepath) as f:
+        return f.read()
+
+
+def main():
+    get_book_text(books / frankenstein.txt)
+
+
+main()
